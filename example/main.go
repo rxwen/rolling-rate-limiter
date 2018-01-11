d97401171cb83887cf2e2681c47947cf4eadfdd4
Raymond Wen <rx.wen218@gmail.com>
2018-01-11 12:11:21 +0800

    fix compilation error

--- example/main.go
+++ example/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/rxwen/rolling-rate-limiter"
 	"time"
+
+	"github.com/rxwen/resourcepool/redispool"
+	"github.com/rxwen/rolling-rate-limiter"
 )
 
 func main() {
-	r := ratelimiter.NewRedisRollingRateLimiter("172.16.154.128:6379", 20, 10)
+	redisPool, _ := redispool.CreateRedisConnectionPool("192.168.2.175:6379", 10, 5)
+	r := ratelimiter.NewRedisRollingRateLimiter("test", redisPool, 20, 10)
 	for i := 0; i < 4; i++ {
 		time.Sleep(1 * time.Second)
 		fmt.Println(r.Check("ip_address_of_client"))
